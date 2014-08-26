a12a06e5a09966a62cc9e4e57be8bd237c101cca
outofdatepie <iliketosmile123@gmail.com>
2014-08-25 20:28:22 -0400

    i did the age calculator

---
+++ Pie is good.py
@@ -0,0 +1,21 @@
+print('''
+Welcome to the age calculator
+''')
+
+
+print('what is your name?')
+name1 = input()
+print (name1)
+
+name2 = input('What is the other persons name?')
+            
+name3 = input('das so cool')
+
+age1 =input('so Gabe how old r u?')
+age1 = int(age1)
+print(age1)
+name4 =input('Thats cool to')
+age2 =input('how old is your friend')
+name5 =input('wow we should hang out some time')
+
+
