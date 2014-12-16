2b56410bc925e5c35e5bf7f9694c9b379cc46c9f
outofdatepie <iliketosmile123@gmail.com>
2014-12-15 17:30:50 -0800

    Quiz

---
+++ QUIZ game.py
@@ -0,0 +1,13 @@
+print('Welcome to the quiz game! Whats your name?')
+name = input('<')
+print ('ok',name,'lets get started')
+
+
+print('So what is the capital of N.C?')
+answer = input('<')
+if answer =='Raleigh':    
+    print('Correct')
+elif answer =='Charlotte':
+    print ('I LUV U')
+else:
+    print('Incorrect')
